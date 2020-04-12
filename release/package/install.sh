779d0e1d85ded3e5f0f85f992b6810ac1b86012c
Sandro <sandrek@gmail.com>
2020-04-12 16:47:49 +0200

    update install.sh like bash.sh

--- release/package/install.sh
+++ release/package/install.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-echo $(date) >> /tmp/jitsi-meet-install.log
+cd /usr/share/jitsi-meet/images
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/images/watermark.png
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/images/favicon.ico
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/images/fuebilogo.png
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/images/fuebilogo_square.png
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/images/shots.gif
+cd /usr/share/jitsi-meet/libs
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/alwaysontop.min.js
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/analytics-ga.js
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/app.bundle.min.js
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/device_selection_popup_bundle.min.js
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/dial_in_info_bundle.min.js
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/do_external_connect.min.js
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/external_api.min.js
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/flacEncodeWorker.min.js
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/lib-jitsi-meet.min.js
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/libflac4-1.3.2.min.js
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/rnnoise-processor.min.js
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/libs/video-blur-effect.min.js
+cd /usr/share/jitsi-meet/css
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/css/all.css
+cd /config
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/interface_config.js
+cd /defaults
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/interface_config.js
+cd /usr/share/jitsi-meet
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/title.html
+curl -O https://raw.githubusercontent.com/saendu/fuebiapp/master/release/0.5/jitsi-meet/interface_config.js
